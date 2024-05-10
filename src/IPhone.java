45617177106f2ef4d1a659c70cde5b4bdfb19873
Debora Cristina <deboracristinasantos5@gmail.com>
2024-05-09 21:09:25 -0300

    Desafio do IPhone.

---
+++ src/IPhone.java
@@ -0,0 +1,47 @@
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    private String marca;
+    private String modelo;
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música...");    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música...");    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando música: " + musica);
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: ");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba...");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página...");
+    }
+}
